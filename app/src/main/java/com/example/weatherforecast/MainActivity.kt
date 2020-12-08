da50b685ce0974ddd752d21c39d882186cd7fc98
kevsierras <kevinsierras96@gmail.com>
2020-12-07 19:15:35 -0800

    Added the API key and City

--- app/src/main/java/com/example/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/MainActivity.kt
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    val CITY: String = "Fullerton, CA"
+    val API: String = "34d8cc8dab66bc691374d628684b5769"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
