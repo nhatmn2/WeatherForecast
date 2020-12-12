33f4b2617820e00f993c3941b3093521c1286af1
nhatmn2 <38064467+nhatmn2@users.noreply.github.com>
2020-12-12 12:57:07 -0800

    adding icon images

--- app/src/main/java/com/example/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/MainActivity.kt
@@ -16,8 +16,7 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     val CITY: String = "fullerton,usa" //location must be in this format
-    val API: String = "34d8cc8dab66bc691374d628684b5769"
-
+    val API: String = "299235b65fa5adf22277b14073b50c40"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -37,9 +36,7 @@ class MainActivity : AppCompatActivity() {
         override fun doInBackground(vararg params: String?): String? {
             var response: String?
             try {
-                response = URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API").readText(
-                        Charsets.UTF_8
-                )
+                response = URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API").readText(Charsets.UTF_8)
             } catch (e: Exception) {
                 response = null
             }
